0dab8a2d807dc0023e16619d02c1535e1e952486
Manuel Guillen <manuel.guillen999@gmail.com>
2023-12-22 00:13:19 -0800

    Problem 2 (Part I) completed

---
+++ Problem02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Part 1
+// ========================
+int GetGameId(string game) => int.Parse(game[5..game.IndexOf(':')]);
+bool IsValidGame(string game) => game[(game.IndexOf(':') + 1)..].Split(';').All(IsValidRoll);
+bool IsValidRoll(string roll) => roll.Split(',').Select(s => s.Trim()).All(IsValidColorPile);
+
+bool IsValidColorPile(string colorPile) => colorPile.Split(' ') switch
+{
+    [string count, "red"] => int.Parse(count) <= 12,
+    [string count, "green"] => int.Parse(count) <= 13,
+    [string count, "blue"] => int.Parse(count) <= 14,
+    _ => false
+};
+
+int result = File.ReadAllLines("input.txt").Where(IsValidGame).Select(GetGameId).Sum();
+Console.WriteLine(result);
