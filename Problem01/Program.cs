164b9f20347afc032b76b0108941eb1a7fabd62f
Manuel Guillen <manuel.guillen999@gmail.com>
2023-12-21 18:09:21 -0800

    Problem 1 (Part I) completed

---
+++ Problem01/Program.cs
@@ -0,0 +1,6 @@
+﻿// Problem 1
+
+static int ExtractValue(string line) => 10*(line.First(Char.IsDigit) - '0') + line.Last(Char.IsDigit) - '0';
+
+int result = File.ReadAllLines("input.txt").Select(ExtractValue).Sum();
+Console.WriteLine(result);
