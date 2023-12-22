1039e28ae0e6903bf31a9f023e6d2a2bf328e46a
Manuel Guillen <manuel.guillen999@gmail.com>
2023-12-22 15:16:41 -0800

    Problem 3 (Part I) completed

---
+++ Problem03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+// Part 1
+// ========================
+HashSet<(int,int)> partPositions = 
+    File.ReadLines("input.txt")
+    .SelectMany((line, rowNum) => Regex.Matches(line, @"[^.\d]").Select(match => (rowNum, match.Index)))
+    .ToHashSet();
+
+bool IsTouchingPart((string, (int, int)) num) => GetSurroundingPoints(num).Any(partPositions.Contains);
+
+IEnumerable<(int, int)> GetSurroundingPoints((string str, (int r, int c) point) num)
+{
+    for (int i = 0; i < num.str.Length+2; i++)
+    {
+        yield return (num.point.r - 1, num.point.c - 1 + i);
+        yield return (num.point.r + 1, num.point.c - 1 + i);
+    }
+    yield return (num.point.r, num.point.c - 1);
+    yield return (num.point.r, num.point.c + num.str.Length);
+}
+
+int result =
+    File.ReadLines("input.txt")
+    .SelectMany((line, rowNum) => Regex.Matches(line, @"\d+").Select(match => (match.Value, (rowNum, match.Index))))
+    .Where(IsTouchingPart)
+    .Select(t => int.Parse(t.Value))
+    .Sum();
+
+Console.WriteLine(result);
