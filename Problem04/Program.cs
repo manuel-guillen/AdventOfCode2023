516bd9dfdd9d986aba1a809532080cdc414491fa
Manuel Guillen <manuel.guillen999@gmail.com>
2023-12-23 13:35:18 -0800

    Problem 4 (Part I) completed

---
+++ Problem04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Part 1
+// ========================
+static int CountWinningNumbers(string str) =>
+    str[(str.IndexOf(':') + 2)..].Split(" | ")
+    .Select(list => list.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToHashSet())
+    .Aggregate((s1, s2) => s1.Intersect(s2).ToHashSet())
+    .Count;
+
+int result = File.ReadLines("input.txt").Select(CountWinningNumbers).Select(n => (int)Math.Pow(2, n-1)).Sum();
+Console.WriteLine(result);
