703537e448a7a7a6a8c2bf98ca88df2458fb43c4
Manuel Guillen <manuel.guillen999@gmail.com>
2023-12-27 12:22:43 -0800

    Problem 5 (Part I) improved

--- Problem05/Program.cs
+++ Problem05/Program.cs
@@ -1,14 +1,32 @@
-﻿static uint PerformMapping(uint n, uint[][] mapping)
+// Part 1
+// ========================
+static IEnumerable<uint[]> MapComposition(uint[][] f, uint[][] g)
 {
-    var range = mapping.FirstOrDefault(range => range[1] <= n && n - range[1] < range[2], [0, 0, 0]);
-    return range[0] + (n - range[1]);
+    var criticalPoints1 = f.SelectMany(r => new[] { r[1], r[1] + r[2] });
+    var criticalPoints2 = g.SelectMany(r => new[] { r[1], r[1] + r[2] }).Select(n => MapEval(n, f, inverse: true));
+    var criticalPoints = criticalPoints1.Union(criticalPoints2).Order().SkipWhile(n => n == 0);
+
+    uint point = 0;
+    foreach (uint nextPoint in criticalPoints)
+    {
+        uint value = MapEval(MapEval(point, f), g);
+        if (value != point) yield return new[] { value, point, nextPoint - point };
+        point = nextPoint;
+    }
 }
 
-static string FeedNumbersIntoMap(string numbers, string map)
+static uint MapEval(uint n, uint[][] mapping, bool inverse = false)
 {
-    uint[][] mapping = map.Split("\n").Select(row => row.Split(" ").Select(uint.Parse).ToArray()).ToArray();
-    return string.Join(" ", numbers.Split(" ").Select(uint.Parse).Select(n => PerformMapping(n, mapping)));
+    int i = inverse ? 1 : 0;
+    var range = mapping.FirstOrDefault(range => range[1-i] <= n && n - range[1-i] < range[2], [0, 0, 0]);
+    return range[i] + (n - range[1-i]);
 }
 
-uint result = File.ReadAllText("input.txt").Split("\n\n").Select(s => s[(s.IndexOf(':')+2)..]).Aggregate(FeedNumbersIntoMap).Split(" ").Select(uint.Parse).Min();
+Func<string,uint[]> ParseRow = row => row.Split(" ").Select(uint.Parse).ToArray();
+
+var input = File.ReadAllText("input.txt").Trim().Split("\n\n").Select(s => s[(s.IndexOf(':') + 2)..]);
+uint[] seeds = ParseRow(input.First());
+uint[][] finalMap = input.Skip(1).Select(s => s.Split("\n").Select(ParseRow).ToArray()).Aggregate((f,g) => MapComposition(f,g).ToArray()).ToArray();
+
+uint result = seeds.Select(n => MapEval(n, finalMap)).Min();
 Console.WriteLine(result);
