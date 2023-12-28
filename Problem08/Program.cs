65152665d213e51ef247c3fa8860e2de59f3cd4e
Manuel Guillen <manuel.guillen999@gmail.com>
2023-12-28 14:18:29 -0800

    Problem 8 (Part I) completed

---
+++ Problem08/Program.cs
@@ -0,0 +1,8 @@
+﻿// Part 1
+// ========================
+int[] steps = File.ReadLines("input.txt").First().Select(c => c == 'L' ? 0 : 1).ToArray();
+var map = File.ReadLines("input.txt").Skip(2).ToDictionary(s => s[0..3], s => s[7..15].Split(", "));
+
+int stepCount = 0;
+for ((string loc, int i) = ("AAA", 0); loc != "ZZZ"; loc = map[loc][steps[i++ % steps.Length]]) stepCount++;
+Console.WriteLine(stepCount);
