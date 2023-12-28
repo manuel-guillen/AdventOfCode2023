c9f69c081b6485ede9d6f4bcd5670d0d86dc6ee6
Manuel Guillen <manuel.guillen999@gmail.com>
2023-12-28 00:20:40 -0800

    Problem 6 (Part II) completed

--- Problem06/Program.cs
+++ Problem06/Program.cs
@@ -1,7 +1,13 @@
 ﻿// Part 1
 // ========================
-static int CountWaysToBeat(int time, int distance) => Enumerable.Range(0, time + 1).Select(t => t * (time - t)).Where(d => d > distance).Count();
+static long CountWaysToBeat(long T, long D) => T - 2*(long)(0.5 * T - 0.5*Math.Sqrt(T * T - 4 * D)) - 1;
 
-int[][] input = File.ReadLines("input.txt").Take(2).Select(s => s.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1..].Select(int.Parse).ToArray()).ToArray();
-int result = Enumerable.Range(0, input[0].Length).Select(i => CountWaysToBeat(input[0][i], input[1][i])).Aggregate((m, n) => m * n);
+var input = File.ReadLines("input.txt").Take(2).Select(s => s.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1..].Select(int.Parse).ToArray()).ToArray();
+var result = Enumerable.Range(0, input[0].Length).Select(i => CountWaysToBeat(input[0][i], input[1][i])).Aggregate((m, n) => m * n);
 Console.WriteLine(result);
+
+// Part 2
+// ========================
+var input2 = File.ReadLines("input.txt").Take(2).Select(s => s.Replace(" ", "")[(s.IndexOf(':') + 1)..]).Select(long.Parse).ToArray();
+var result2 = CountWaysToBeat(input2[0], input2[1]);
+Console.WriteLine(result2);
