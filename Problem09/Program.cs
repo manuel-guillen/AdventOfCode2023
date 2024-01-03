08a9af051b7f2a32c3f059baa8233521cd18d277
Manuel Guillen <manuel.guillen999@gmail.com>
2024-01-03 14:13:29 -0800

    Problem 9 (Part II) completed

--- Problem09/Program.cs
+++ Problem09/Program.cs
@@ -5,3 +5,10 @@ int ExtrapolateNext(int[] seq) => seq.All(x => x == seq[0]) ? seq[0] : seq[^1] +
 
 var result = File.ReadLines("input.txt").Select(s => s.Split(' ').Select(int.Parse).ToArray()).Select(ExtrapolateNext).Sum();
 Console.WriteLine(result);
+
+// Part 2
+// ========================
+int ExtrapolatePrev(int[] seq) => seq.All(x => x == seq[0]) ? seq[0] : seq[0] - ExtrapolatePrev(Differences(seq));
+
+var result2 = File.ReadLines("input.txt").Select(s => s.Split(' ').Select(int.Parse).ToArray()).Select(ExtrapolatePrev).Sum();
+Console.WriteLine(result2);
