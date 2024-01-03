76919a88f53d353cf852e2040a1f7f6782d81f04
Manuel Guillen <manuel.guillen999@gmail.com>
2024-01-03 14:09:56 -0800

    Problem 9 (Part I) completed

---
+++ Problem09/Program.cs
@@ -0,0 +1,7 @@
+﻿// Part 1
+// ========================
+int[] Differences(int[] seq) => seq.Zip(seq.Skip(1), (a, b) => b - a).ToArray();
+int ExtrapolateNext(int[] seq) => seq.All(x => x == seq[0]) ? seq[0] : seq[^1] + ExtrapolateNext(Differences(seq));
+
+var result = File.ReadLines("input.txt").Select(s => s.Split(' ').Select(int.Parse).ToArray()).Select(ExtrapolateNext).Sum();
+Console.WriteLine(result);
